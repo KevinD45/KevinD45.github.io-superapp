52472cbb763d8c0d96a223aa6eb00a314cffe649
KevinD45 <78800641+KevinD45@users.noreply.github.com>
2021-03-01 11:43:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+$(document).ready(function(){
+    $(".propos").mouseenter(function(){
+        $(this).css("background-color", "black");
+    });
+
+    $(".propos").mouseleave(function(){
+        $(this).css("background-color", "#DD5555");
+    });
+});
+
+
+
+$(document).ready(function(){
+    $(".service").mouseenter(function(){
+        $(this).css("background-color", "black");
+    });
+
+    $(".service").mouseleave(function(){
+        $(this).css("background-color", "#DD5555");
+    });
+});
+
+
+
+$(document).ready(function(){
+    $(".equipe").mouseenter(function(){
+        $(this).css("background-color", "black");
+    });
+
+    $(".equipe").mouseleave(function(){
+        $(this).css("background-color", "#DD5555");
+    });
+});
+
+
+
+$(document).ready(function(){
+    $(".contactUn").mouseenter(function(){
+        $(this).css("background-color", "black");
+    });
+
+    $(".contactUn").mouseleave(function(){
+        $(this).css("background-color", "#DD5555");
+    });
+});
+
+
+$(document).ready(function(){
+    $(".propos").mouseenter(function(){
+        $(this).css("background-color", "black");
+    });
+
+    $(".propos").mouseleave(function(){
+        $(this).css("background-color", "#DD5555");
+    });
+});
+
+
+
+$(document).ready(function(){
+    $("#conteneur").mouseenter(function(){
+        $(this).css("background-color", "lightBlue");
+    });
+
+    $("#conteneur").mouseleave(function(){
+        $(this).css("background-color", "#fff");
+    });
+});
+
+
+
+$(document).ready(function(){
+    $("#conteneur1").mouseenter(function(){
+        $(this).css("background-color", "lightBlue");
+    });
+
+    $("#conteneur1").mouseleave(function(){
+        $(this).css("background-color", "#fff");
+    });
+});
+
+
+
+
